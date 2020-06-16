ef21ed4ac5ad9e6de6e3aa28886742ced1e28ba1
aleks <aleksandr.law@gmail.com>
2020-06-16 13:08:24 +0300

    160620 1308

--- src/main/java/main/controller/mainControllerRest.java
+++ src/main/java/main/controller/mainControllerRest.java
@@ -1,22 +1,16 @@
 package main.controller;
 
-import main.model.Role;
 import main.model.User;
 import main.service.ServiceUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashSet;
-import java.util.Set;
-
 @RestController
 public class mainControllerRest {
 
     @Autowired
     private ServiceUser serviceUser;
-//    @Autowired
-//    private PasswordEncoder passwordEncoder;
 
     @GetMapping("/all")
     public Iterable<User> allUser() {
@@ -31,25 +25,8 @@ public class mainControllerRest {
 
     @PostMapping("/edit")
     public User update(@RequestBody User user) {
-//        Long id = Long.parseLong(idd);
-//        int ageInt = Integer.parseInt(age);
-//        HashSet<Role> roles = (HashSet<Role>) getRoles(userRoles);
-//        User byName = serviceUser.findById(id).get();
-//        if (!password.equals("")) {
-//            byName.setPassword(password);
-//
-//            //  byName.setPassword(passwordEncoder.encode(password));
-//        }
-//        byName.setId(id);
-//        byName.setName(name);
-//        byName.setLastName(lastName);
-//        byName.setAge(ageInt);
-//        byName.setEmail(email);
-//        byName.setUserRoles(roles);
-
         return serviceUser.save(user);
     }
-
 
     @PostMapping("/delete")
     public HttpStatus delete(@RequestParam("id") String idd) {
@@ -69,15 +46,6 @@ public class mainControllerRest {
         Long id = Long.parseLong(idd);
         User byId = serviceUser.findById(id).get();
         return byId;
-    }
-
-    private Set<Role> getRoles(@RequestParam("role") String role) {
-        Set<Role> userRoles = new HashSet<>();
-        String[] split = role.split(",");
-        for (String s : split) {
-            userRoles.add(Role.valueOf(s));
-        }
-        return userRoles;
     }
 
     @PostMapping("/getByName")
